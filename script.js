7d01f651469039072eb82065fd97aeecf9015416
nicgmas <guimaraes.nicolly2007@gmail.com>
2026-09-15 10:14:34 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-const form = document.getElementById('newsForm');
-  const status = document.getElementById('newsStatus');
-  form.addEventListener('submit', function(e){
-    e.preventDefault();
-    status.textContent = 'Pronto — você será avisado por e-mail.';
-    form.reset();
-  });
+const form = document.getElementById("newsForm");
+const status = document.getElementById("newsStatus");
+
+form.addEventListener("submit", function (event) {
+
+  event.preventDefault();
+
+  status.textContent =
+    "Pronto — você será avisado quando houver novidades.";
+
+  form.reset();
+
+});
